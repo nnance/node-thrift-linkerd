b44010d7eee5a7de275a7335b3da55c70120312d
nnance <nance.nick@gmail.com>
2016-11-15 07:49:09 -0800

    numbers return response

--- svc-numbers/src/thrift/numbers.thrift
+++ svc-numbers/src/thrift/numbers.thrift
@@ -1,6 +1,11 @@
 namespace java com.twitter.finagle.example.thriftjava
 #@namespace scala com.twitter.finagle.example.thriftscala
 
+struct GenerateResponse {
+    1: required i32 id,
+}
+
+
 service Numbers {
-  i32 generate();
+  GenerateResponse generate();
 }
